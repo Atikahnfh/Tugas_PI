9b6e35879a84fbc87edee7b58f749453a25db7c0
mafifanarss <sm.afifanaly@gmail.com>
2023-05-26 09:12:41 +0700

    update sendtokencontroller

--- api-mahasiswa/app/Http/Controllers/SendTokenController.php
+++ api-mahasiswa/app/Http/Controllers/SendTokenController.php
@@ -6,7 +6,6 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Traits\HttpResponses;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 
 class SendTokenController extends Controller
@@ -20,22 +19,21 @@ class SendTokenController extends Controller
 
     public function viewData(Request $request)
     {
-
         $validator = $request->validate([
             'email' => 'required|email:dns|unique:users',
             'name' => 'required|min:3|max:100',
             'purpose' => 'required|min:8'
         ]);
-
-        $user = Http::withHeaders([
-                        'Accept' => 'application/vnd.api+json',
-                        'Content-Type' => 'application/vnd.api+json'])
-                    ->post('https://api-mahasiswa.scholarfy.site/register', [
-                        'name' => $request->name,
-                        'email' => $request->email,
-                        'password' => $request->email,
-                        'password_confirmation' => $request->email])->json();
         
-        return $user;
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->email)
+        ]);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('API Token of ' . $user->name)->plainTextToken
+        ]);
     }
 }
