57674f02d09faba49c7eabc2acd78d8a9315a736
mafifanarss <sm.afifanaly@gmail.com>
2023-06-02 10:52:52 +0700

    merapikan proses generate token baik tampilan dengan tailwindcss dan controller

--- resources/views/generate-token.blade.php
+++ resources/views/generate-token.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>API Mahasiswa</title>
+    <title>API Beasiswa</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script>
         if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
